7968dc54b8b3ba0ae21322d89538746fbbad49cf
Ferran Biayna <ferranbiaynacosta@gmail.com>
2022-05-21 18:30:45 +0200

    Added DEV and PROD config in Android

--- android/app/src/main/java/com/todolist/newarchitecture/components/MainComponentsRegistry.java
+++ android/app/src/main/java/com/todolist/newarchitecture/components/MainComponentsRegistry.java
@@ -1,4 +1,4 @@
-package com.todolist.newarchitecture.components;
+package com.todolist.app.newarchitecture.components;
 
 import com.facebook.jni.HybridData;
 import com.facebook.proguard.annotations.DoNotStrip;
